8c053d794cc4ec9b207831eae6789602246fb4fc
BuildTools <unconfigured@null.spigotmc.org>
2023-01-08 21:33:53 -0800

    Removed immature connection and audio player destruction

--- commands/loselp.js
+++ commands/loselp.js
@@ -2,55 +2,31 @@ const { SlashCommandBuilder } = require('discord.js');
 const { StreamType, createAudioPlayer, createAudioResource, joinVoiceChannel, NoSubscriberBehavior, VoiceConnectionStatus, entersState, AudioPlayerStatus, getVoiceConnection } = require('@discordjs/voice');
 const { createReadStream } = require('node:fs');
 
-
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('loselp')
         .setDescription('Plays life point sound'),
 
     async execute(interaction){
-        const player = createAudioPlayer({
-            behaviors: {
-            },
-        });
-        console.log(interaction.voice);
-        
-        const resource = createAudioResource(createReadStream('./sounds/lpsound.ogg'), {
-            inputType: StreamType.OggOpus,
-            metadata: {
-                title: 'Life Point Loss',
-            },
-        });
-
-        player.on('error', error => {
-            console.error(`Error: ${error.message} with resource ${error.resource.metadata.title}`);
-        });
-
-        player.on(AudioPlayerStatus.Playing, () => {
-            console.log('The audio player has started playing!');
-        });
 
         const channel = interaction.member.voice.channel;
         if(!channel) return interaction.channel.send('Please join a Voice Channel first');
 
-        const connection = joinVoiceChannel({
+        const voiceConnection = joinVoiceChannel({
             channelId: channel.id,
             guildId: interaction.guild.id,
             adapterCreator: interaction.guild.voiceAdapterCreator,
         });
-        
-        connection.on('error', error => {
-            console.error(`${error.message}`);
+
+        voiceConnection.on('stateChange', (oldState, newState) => {
+            console.log(`Connection transitioned from ${oldState.status} to ${newState.status}`);
         });
 
-        player.play(resource);
-        connection.subscribe(player);
-
-        connection.on(VoiceConnectionStatus.Ready, (oldState, newState) => {
+        voiceConnection.on(VoiceConnectionStatus.Ready, (oldState, newState) => {
             console.log('Connection is in the Ready state!');
         });
 
-        connection.on(VoiceConnectionStatus.Disconnected, async (oldState, newState) => {
+        voiceConnection.on(VoiceConnectionStatus.Disconnected, async (oldState, newState) => {
             try{
                 await Promise.race([
                     entersState(connection, VoiceConnectionStatus.Signalling, 5_000),
@@ -60,13 +36,41 @@ module.exports = {
                 connection.destroy();
             }
         })
+
+        const player = createAudioPlayer({
+            behaviors: {
+                NoSubscriber: NoSubscriberBehavior.Stop,
+            },
+        });
+
+        player.on('error', error => {
+            console.error(`Error: ${error.message} with resource ${error.resource.metadata.title}`);
+        });
+
+        player.on('stateChange', (oldState, newState) => {
+            console.log(`Audio player transitioned from ${oldState.status} to ${newState.status}`);
+        });
+
+        player.on(AudioPlayerStatus.Idle, async (oldState, newState) => {
+            try{
+                await Promise.race([
+                    entersState(connection, AudioPlayerStatus.Playing, 30_000),
+                ]);
+            } catch (error) {
+                player.stop();
+            }
+        });
+
+        const resource = createAudioResource(createReadStream('sounds/lpsound.ogg', {
+            inputType: StreamType.OggOpus,
+        }));
+
+        voiceConnection.subscribe(player);
+        player.play(resource);
+        //await interaction.reply('Taking damage');
         
-        /*
-        player.on(AudioPlayerStatus.Idle, () => {
+        /*player.on(AudioPlayerStatus.Idle, () => {
             player.play(getNextResource());
         });*/
-
-        player.stop();
-        connection.destroy();
     },
-};
+}; 
