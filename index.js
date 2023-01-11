3cca63385de5115ddfc2bc086b35073b5059f7d7
tehbran <brandonknguyen2@gmail.com>
2023-01-11 10:08:25 -0800

    Added comments

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ const fs = require('node:fs');
 const { Client, Collection, Events, GatewayIntentBits, PermissionsBitField } = require('discord.js');
 const { VoiceConnectionStatus, AudioPlayerStatus } = require('@discordjs/voice');
 const path = require('node:path');
+
+//Edit config_template.json and rename it or change this for the bot's login credentials
 const { token } = require('./config.json');
 
+//Generates permissions for the bot
 const client = new Client({ 
     intents: [ 
         GatewayIntentBits.Guilds,
@@ -13,11 +16,12 @@ const client = new Client({
      ]
      });
 
+//Generates a list of commands the bot will listen for, with their filepaths
 client.commands = new Collection();
-
 const commandsPath = path.join(__dirname, 'commands');
 const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
 
+//Makes sure the slash commands are actually slash commands
 for (const file of commandFiles) {
     const filePath = path.join(commandsPath, file);
     const command = require(filePath);
@@ -29,10 +33,12 @@ for (const file of commandFiles) {
     }
 }
 
+//Confirms that bot is ready to handle Events
 client.once(Events.ClientReady, c=> {
     console.log(`Ready! logged in as ${c.user.tag}`);
 });
 
+//Event handle for commands
 client.on(Events.InteractionCreate, async interaction => {
     if (!interaction.isChatInputCommand()) return;
 
@@ -51,4 +57,5 @@ client.on(Events.InteractionCreate, async interaction => {
     }
 });
 
+//Always put at the end, logs the bot into Discord
 client.login(token);
